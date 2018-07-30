c8ddd815ccaa60d9c7e3e32d3f1d18d1925fe86b
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-07-30 12:38:59 -0400

    updating rounding to whole numbers

--- SpotCheck/wwwroot/js/site.js
+++ SpotCheck/wwwroot/js/site.js
@@ -22,11 +22,11 @@ function UpdateValues() {
         var soundOfInterestLevel = 10.0 * Math.log10(
                                             Math.pow(10,totalSoundLevel / 10.0) -
                                             Math.pow(10,backgroundOnlySoundLevel / 10.0));
-        $("#soundOfInterestLevel").val(Math.round(soundOfInterestLevel * 100) / 100);
+        $("#soundOfInterestLevel").val(Math.round(soundOfInterestLevel));
 
         if (distFromSound && distFromSound >= 0) {
             var estSoundLevel = Math.round(85 - 20 * Math.log10(distFromSound / 52));
-            $("#estSoundLevel").val(Math.round((estSoundLevel * 100) / 100));
+            $("#estSoundLevel").val(Math.round((estSoundLevel)));
         }
         else {
             $("#estSoundLevel").val("");
