931224ea1f96adf5b2002503c8598f84b7d57f3a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-07-30 11:21:52 -0400

    fixing events

--- SpotCheck/wwwroot/js/site.js
+++ SpotCheck/wwwroot/js/site.js
@@ -3,16 +3,16 @@
 $(".userinput").change(function () {
     UpdateValues();
 });
-
+$(".userinput").keyup(function () {
+    UpdateValues();
+});
 
 function UpdateValues() {
     // check to see if all inputs are filled in for backgound sound level and update if so
-    //console.log('UpdateValues called');
     var totalSoundLevel = parseInt($("#totalSoundLevel").val());
     var backgroundOnlySoundLevel = parseInt($("#backgroundOnlySoundLevel").val());
     var distFromSound = parseInt($("#distFromSound").val());
-
-    //console.log("totalSoundLevel: " + totalSoundLevel + " backgroundOnlySoundLevel:" + backgroundOnlySoundLevel + " distFromSound:" + distFromSound);
+    // console.log("updatevalues. totalSoundLevel:" + totalSoundLevel + " backgroundOnlySoundLevel:" + backgroundOnlySoundLevel + " distFromSound:" + distFromSound);
 
     if (!totalSoundLevel || !backgroundOnlySoundLevel ||       
         totalSoundLevel < 0 || backgroundOnlySoundLevel < 0) {
