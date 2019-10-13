3e00e275d65237f15071b1cb815d23fa2250738a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-10-12 23:13:40 -0400

    adding new project using Pages for 3.0 version

---
+++ SpotCheckPages/wwwroot/js/site.js
@@ -0,0 +1,104 @@
+﻿// Write your JavaScript code.
+
+
+window.addEventListener('load', async e => {
+    console.log('load event');
+    if ('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('sw.js');
+            console.log('SW registered');
+        }
+        catch (error) {
+            console.log('error');
+        }
+    }
+});
+
+
+
+$(".userinput").change(function () {
+    UpdateValues();
+});
+$(".userinput").keyup(function () {
+    UpdateValues();
+});
+$("#resetButton").click(function () {
+    ResetValues();
+});
+function UpdateValues() {
+    // check to see if all inputs are filled in for backgound sound level and update if so
+    var totalSoundLevel = parseInt($("#totalSoundLevel").val());
+    var backgroundOnlySoundLevel = parseInt($("#backgroundOnlySoundLevel").val());
+    var distFromSound = parseInt($("#distFromSound").val());
+    // console.log("updatevalues. totalSoundLevel:" + totalSoundLevel + " backgroundOnlySoundLevel:" + backgroundOnlySoundLevel + " distFromSound:" + distFromSound);
+    var soundOfInterestLevel;
+    var estSoundLevel;
+    if (!totalSoundLevel || !backgroundOnlySoundLevel ||       
+        totalSoundLevel <= 0 || backgroundOnlySoundLevel <= 0) {
+        $("#soundOfInterestLevel").val("");
+    }
+    else {
+        soundOfInterestLevel = 10.0 * Math.log10(
+                                            Math.pow(10,totalSoundLevel / 10.0) -
+                                            Math.pow(10,backgroundOnlySoundLevel / 10.0));
+        $("#soundOfInterestLevel").val(Math.round(soundOfInterestLevel));
+
+        if (distFromSound && distFromSound >= 0) {
+            estSoundLevel = Math.round(85 - 20 * Math.log10(distFromSound / 52));
+            $("#estSoundLevel").val(Math.round((estSoundLevel)));
+        }
+        else {
+            $("#estSoundLevel").val("");
+        }
+    }
+
+    UpdateResultMessageArea(estSoundLevel, soundOfInterestLevel);
+}
+
+function UpdateResultMessageArea(estSoundLevel, soundOfInterest) {
+    // console.log("URMA: estSoundLevel:" + estSoundLevel + " soundOfInterest:" + soundOfInterest);
+    const resultOkMessage = "Estimated sound level and Sound of Interest are within 10 dBA which is expected.";
+    const resultTooNegativeMessage = "Estimated Sound Level is more than 10 dBA HIGHER THAN Sound of Interest Level. Check your measurement location. There may be a barrier or other object blocking the sound path.";
+    const resultTooPositiveMessage = "Estimated Sound Level is less than 10 dBA LOWER THAN Sound of Interest Level. Check your measurement location. There may be a sound source close to your measurement locations that is influencing the results.";
+
+    var resultMessage = $("#resultMessage");
+    if (!estSoundLevel || !soundOfInterest || estSoundLevel <= 0) {
+        resultMessage.css("display", "none");
+    }
+    else {
+        if (Math.abs(estSoundLevel - soundOfInterest) <= 10) {
+            $("#resultMessageText").text(resultOkMessage);
+            $("#resultIcon").attr("src", "/images/check-32.png");
+        }
+        else if (estSoundLevel < soundOfInterest) {
+            $("#resultMessageText").text(resultTooNegativeMessage);
+            $("#resultIcon").attr("src", "/images/warning-32.png");
+        }
+        else {
+            $("#resultMessageText").text(resultTooPositiveMessage);
+            $("#resultIcon").attr("src", "/images/warning-32.png");
+        }
+
+        resultMessage.css("display", "block");
+    }
+
+
+    /*
+     *  private const string ResultOkMessage = @"Estimated sound level and Sound of Interest are within 10 dBA which is expected.";
+		private const string ResultsTooNegativeMessage = @"Estimated Sound Level is more than 10 dBA HIGHER THAN Sound of Interest Level. Check your measurement location. There may be a barrier or other object blocking the sound path.";
+        private const string ResultsTooPositiveMessage = @"Estimated Sound Level is less than 10 dBA LOWER THAN Sound of Interest Level. Check your measurement location. There may be a sound source close to your measurement locations that is influencing the results.";
+
+     */
+}
+
+function ResetValues() {
+    $("input").val("");
+    UpdateResultMessageArea();
+}
+
+// serviceworker related items
+$("#updateAppButton").click(function () { SendUpdateAppToSw(); });
+function SendUpdateAppToSw() {
+    console.log('SendUpdateAppToSw');
+    navigator.serviceWorker.controller.postMessage("UpdateApp");
+}
---
+++ SpotCheckPages/wwwroot/sw.js
@@ -0,0 +1,94 @@
+﻿
+const staticAssets = [
+    './',
+    './sw.js',
+    './css/site.css',
+    './css/site.min.css',
+    './js/site.js',
+    './js/site.min.js',
+];
+
+const mainCacheName = 'spotcheck-01';
+
+// // https://stackoverflow.com/questions/38168276/navigator-serviceworker-controller-is-null-until-page-refresh
+self.addEventListener('install', async event => {
+    event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', function (event) {
+    event.waitUntil(self.clients.claim()); // Become available to all pages
+});
+
+
+self.addEventListener('fetch', async event => {
+    handleFetch(event);
+});
+
+
+
+async function handleFetch(event) {
+    event.respondWith(cacheFirst(event.request));
+}
+
+async function cacheFirst(request) {
+    try {
+        const cachedResponse = await caches.match(request);
+        if (cachedResponse) {
+            return cachedResponse
+        }
+    }
+    catch (error) {
+        console.debug('error in cacheFirst():'+error);
+    }
+
+    var response = await fetch(request);
+    const cache = await caches.open(mainCacheName);
+    cache.put(request, response.clone());
+
+    return response;
+}
+
+async function networkFirst(request) {
+    console.log('networkFirst');
+    const cache = await caches.open(mainCacheName);
+
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+    }
+    catch (error) {
+        return cache.match(request);
+    }
+}
+
+self.addEventListener('message', async function (event) {
+    console.log("SW Received Message: " + event.data);
+    UpdateApp();
+});
+
+async function UpdateApp() {
+    const cache = await caches.open(mainCacheName);
+
+    try {
+        staticAssets.forEach(function (asset) {
+            cache.delete(asset, {
+                'ignoreSearch': true,
+                'ignoreMethod': true,
+                'ignoreVary': true
+            });
+        });
+    }
+    catch (error) {
+        console.log('error: ' + error);
+    }
+    console.log('removed assets');
+
+    cache.addAll(staticAssets);
+}
+/*
+ * response.forEach(function(element, index, array) {
+      cache.delete(element);
+    });
+ */
+
+
